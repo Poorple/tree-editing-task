addc79fa209db8e0116308b455b6305a1b337bdc
Poorple <91628099+Poorple@users.noreply.github.com>
2024-10-10 03:30:38 +0200

    removed imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import ChildComponent from "./components/ChildComponent";
 import ParentComponent from "./components/ParentComponent";
 import "./styles/parentComponent.scss";
 const App = () => {
